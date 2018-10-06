87b546666be2bb1ccf461801f7393fea82bfd519
Hello HongFu <hellohongfu@outlook.com>
2018-10-06 08:29:31 +0800

    文件夹统一配置，生成的图片放到指定文件夹

--- src/main/java/com/qr/demo/config/WebMvcConfig.java
+++ src/main/java/com/qr/demo/config/WebMvcConfig.java
@@ -1,14 +1,29 @@
 package com.qr.demo.config;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 @Configuration
+@ConfigurationProperties(prefix = "file")
+
 public class WebMvcConfig extends WebMvcConfigurationSupport  {
+
+    private String dirQr;
+	public String getDirQr() {
+     
+
+		return dirQr;
+	}
+	public void setDirQr(String dirQr) {
+		this.dirQr = dirQr;
+	}
+    
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/qr/**").addResourceLocations("file:qrCodeImages/");
+        registry.addResourceHandler("/qr/**").addResourceLocations("file:"+dirQr+"/");
     }
 }
 
